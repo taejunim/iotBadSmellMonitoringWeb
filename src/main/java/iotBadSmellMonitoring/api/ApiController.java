0118fdb76dd9d92d24d20857ea7ce8b9e859d539
J <wognsld01@gmail.com>
2021-06-03 10:32:25 +0900

    #API SERVICE를 위한 패치키 및 Controller 미리 만들기.

---
+++ src/main/java/iotBadSmellMonitoring/api/ApiController.java
@@ -0,0 +1,21 @@
+package iotBadSmellMonitoring.api;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @ Class Name   : ApiController.java
+ * @ Modification : API SERVICE COTROLLER
+ * @
+ * @ 최초 생성일     최초 생성자
+ * @ ---------    ---------
+ * @ 2021.06.03.    고재훈
+ * @
+ * @ 수정일           수정자
+ * @ ---------    ---------
+ * @
+ **/
+
+@Controller
+public class ApiController {
+
+}
